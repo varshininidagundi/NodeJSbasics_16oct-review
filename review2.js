0bce4023ce1afc396604d886d5b8f417fc70dab7
varshini <varshininidagundi2001@gmail.com>
2023-10-23 11:05:51 +0530

    review 2

---
+++ review2.js
@@ -0,0 +1,8 @@
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!'); 
+// Wait for user's response.
+//var userName = readlineSync.question('May I have your name? ');
+//console.log('Hi ' + userName + '!');
